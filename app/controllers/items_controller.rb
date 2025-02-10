fb6dabd6c85e2ff1b4e1291ce9c4d07738fb21a6
Steven Wang <34365342+ShuhengW@users.noreply.github.com>
2025-02-10 19:24:12 +0000

    fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,20 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def backdoor
+    render({ :template => "item_templates/backdoor" })
+  end
+
+  def create
+
+    query_link_url= params.fetch("query_link_url")
+    query_link_description = params.fetch("query_link_description")
+    query_thumbnail_url = params.fetch("query_thumbnail_url")
+
+    Item.create({ :link_url => query_link_url, :link_description => query_link_description, :thumbnail_url => query_thumbnail_url })
+    
+    redirect_to("/")
+  end
+
 end
